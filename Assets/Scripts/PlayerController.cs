11d59cf1fbd67f9c65b9eec1068d9c2ffe3cc0e1
hoodey <41493055+hoodey@users.noreply.github.com>
2024-09-05 20:56:36 -0500

    Updates to player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
-
+    Animator animator;
+    Rigidbody rb;
+    PlayerInput input;
+    [SerializeField] float jumpForce;
+    [SerializeField] float speed;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponentInChildren<Rigidbody>();
+        input = GetComponentInChildren<PlayerInput>();
     }
 
     // Update is called once per frame
@@ -23,12 +28,13 @@ public class PlayerController : MonoBehaviour
 
     public void OnMovement()
     {
-
+        
     }
 
     public void OnJump()
     {
-        Debug.Log("Jumped");
+        rb.AddForce(0.0f, jumpForce, 0.0f, ForceMode.Impulse);
+        animator.SetTrigger("Jump");
     }
 
 
